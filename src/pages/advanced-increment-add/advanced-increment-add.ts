0c2c3116af294e44528cbd5b4437fbb3e0849461
EdgarsJoja <edgarsjoja@gmail.com>
2018-03-06 15:57:09 +0200

    [29] - Add advanced options button to list items - Added momentjs package. - Added ionic default datetime picker element in increment add page.

--- src/pages/advanced-increment-add/advanced-increment-add.ts
+++ src/pages/advanced-increment-add/advanced-increment-add.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
-import { IncrementInterface, IncrementProvider } from '../../providers/increment/increment';
+import moment from 'moment';
+
+import { IncrementInterface } from '../../providers/increment/increment';
 import { ItemInterface } from '../../providers/item/item';
 import { DEFAULT_UNITS } from "../../providers/units/units";
 
@@ -16,22 +17,21 @@ export class AdvancedIncrementAddPage {
     public incrementFormGroup: FormGroup;
     public increment: IncrementInterface;
     public defaultUnits = [];
+    public currentDateTime = '';
 
     private item: ItemInterface;
-
-    private showResetField = false;
-    private showOtherUnitField = false;
 
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
-        public viewCtrl: ViewController,
-        private formBuilder: FormBuilder
+        public viewCtrl: ViewController
     ) {
         this.item = this.navParams.get('item') || <ItemInterface>{};
 
         this.defaultUnits = DEFAULT_UNITS;
-        console.log(this.defaultUnits);
+
+        // To allow select only past dates
+        this.currentDateTime = moment().format('YYYY-MM-DD');
 
         this.increment = <IncrementInterface>{
             id: this.item.id,
